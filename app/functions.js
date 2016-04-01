aa429f0696fd0035b34fb89e1683d8e0d8a8b2c8
Michael Berber <mickberber@icloud.com>
2016-04-01 10:59:42 -0700

    [feat]: complete some functions

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,25 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(this, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   functionFunction : function(str) {
-
+    return function(arg) {
+      return str + ', '+ arg;
+    }
   },
 
   makeClosures : function(arr, fn) {
-
+    var closed = [];
+    for(var i = 0; i < arr.length; i++) {
+      closed[i] = fn.bind(this, arr[i]);
+    }
+    return closed;
   },
 
   partial : function(fn, str1, str2) {
